ab63301b2cd521883e8969a81398f9b7c91f31b3
NicolasXzz <110706585+NicolasXzz@users.noreply.github.com>
2024-10-06 16:33:55 +0700

    Update NonceFinder.js

--- sui_meta_miner/includes/NonceFinder.js
+++ sui_meta_miner/includes/NonceFinder.js
@@ -7,7 +7,7 @@ export default class NonceFinder {
         this._tryingNonces = [];
         this._initialNonce = 0;
         this._startedSearchAt = null;
-        this._workersCount = 32;
+        this._workersCount = 12;
 
         this._askedToStop = false;
 
